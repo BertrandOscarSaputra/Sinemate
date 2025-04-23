45e194d320439eb50ae11dfb27e5496988f7ce48
BertrandOscarSaputra <bertrandosc@gmail.com>
2025-04-23 21:03:24 +0800

    SplashScreen

---
+++ src/pages/LogSignIn/index.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const LogSignIn = () => {
+  return (
+    <View>
+      <Text> LogSignIn</Text>
+    </View>
+  );
+};
+
+export default LogSignIn;
+
+const styles = StyleSheet.create({});
---
+++ src/pages/SplashScreen/index.tsx
@@ -0,0 +1,29 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {Logo} from '../../assets';
+
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => navigation.replace('LogSignIn'), 3000);
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Logo />
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#000000',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 32,
+  },
+});
